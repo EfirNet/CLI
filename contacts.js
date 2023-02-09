220fdc64daf8168bd38593ce5864634428748926
efirnet <efirnet@gmail.com>
2023-02-10 00:36:05 +0200

    add files

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require('fs/promises');
+const path = require('path');
+// const { nanoid } = require('nanoid');
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newId = String(contacts.length + 1);
+  const newContact = {
+    id: newId,
+    // id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const list = JSON.stringify([...contacts, newContact], null, '\t')
+  await fs.writeFile(contactsPath, list);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
